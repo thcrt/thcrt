a0afaa4ec91e6a61b3ffad138612f2c81c4149e3
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-13 22:56:19 +0100

    Add some quotes

--- website/quotes.py
+++ website/quotes.py
@@ -2,4 +2,6 @@ quotes = [
     "style is the answer to everything.",
     "to do a dull thing with style is preferable to doing a dangerous thing without it.",
     "to do a dangerous thing with style is what i call art.",
+    "four shalt thou not count, neither count thou two, excepting that thou then proceed to three. five is right out.",
+    "you must construct additional pylons.",
 ]
