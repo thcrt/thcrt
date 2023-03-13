007e4794120fc085fbb66be061438320c5c2e589
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-13 22:50:43 +0100

    add quotes

--- website/page.py
+++ website/page.py
@@ -1,8 +1,13 @@
+from random import choice
+
 import flask as f
+
+from .quotes import quotes
 
 page = f.Blueprint('page', __name__, template_folder="templates")
 
 
 @page.route("/")
 def home():
-    return f.render_template("home.html")
+    quote = choice(quotes)
+    return f.render_template("home.html", quote=quote)
---
+++ website/quotes.py
@@ -0,0 +1,5 @@
+quotes = [
+    "style is the answer to everything.",
+    "to do a dull thing with style is preferable to doing a dangerous thing without it.",
+    "to do a dangerous thing with style is what i call art.",
+]
