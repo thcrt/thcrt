08efe9674278d316d9c5165c05f7c9d8980e1bbe
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-03 16:08:48 +0200

    Copy service

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")" # go to script dir
 
 git pull
-sudo mv web_waitress.service /etc/systemd/system
+sudo cp web_waitress.service /etc/systemd/system
 tailwindcss -c tailwind.config.js -o website/static/tailwind.css --minify
 sudo systemctl reload caddy
 sudo systemctl restart web_waitress
