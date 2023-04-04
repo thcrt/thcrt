ccfbf8ea8864a31c34330de1116a7d103bdcf3b6
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-04 16:25:03 +0200

    Use Docker woooooo

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")" # go to script dir
-
-git pull
-sudo cp web_waitress.service /etc/systemd/system
-tailwindcss -c tailwind.config.js -o website/static/tailwind.css --minify
-sudo systemctl reload caddy
-sudo systemctl restart web_waitress
