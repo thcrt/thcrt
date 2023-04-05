b606133ce77a4a38d0326155a869057698975eab
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-05 19:40:15 +0200

    Clean up sampling

--- website/page.py
+++ website/page.py
@@ -8,11 +8,15 @@ from .quotes import quotes
 page = f.Blueprint('page', __name__, template_folder="templates")
 
 
+POSTS_ON_HOMEPAGE = 3
+
+
 @page.route("/")
 def home():
     quote = sample(quotes, 1)[0]
+    number_of_posts = len(app.pm.posts)
     posts = sample(
         list(app.pm.posts),
-        3 if len(app.pm.posts) >= 3 else len(app.pm.posts)
+        POSTS_ON_HOMEPAGE if number_of_posts >= POSTS_ON_HOMEPAGE else number_of_posts
     )
     return f.render_template("home.html", quote=quote, posts=posts)
