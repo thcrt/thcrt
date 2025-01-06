4d2bcd2da32ad881999d39ebeded23f84226abe9
thcrt <110127860+thcrt@users.noreply.github.com>
2025-01-06 20:50:39 +0100

    Add colophon

--- src/thcrt/pages.py
+++ src/thcrt/pages.py
@@ -6,3 +6,7 @@ bp = f.Blueprint('pages', __name__)
 @bp.route("/")
 def index():
     return f.render_template("index.html.jinja")
+
+@bp.route("/colophon")
+def colophon():
+    return f.render_template("colophon.html.jinja")
